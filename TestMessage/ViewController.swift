618c900e5adb5de31b4f9478e9bacd2996d449ad
rolodestar <39199934@qq.com>
2020-08-08 15:44:18 +0800

    Initial Commit

---
+++ TestMessage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestMessage
+//
+//  Created by rolodestar on 2020/8/8.
+//  Copyright © 2020 Rolodestar Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
